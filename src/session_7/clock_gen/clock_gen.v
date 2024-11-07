0638b62077547f4a1c9221db616361d6ba8c79cc
William Woo <codefoolosopher@gmail.com>
2024-11-08 00:16:53 +0900

    Revised session #7

--- src/session_7/clock_gen/clock_gen.v
+++ src/session_7/clock_gen/clock_gen.v
@@ -2,14 +2,16 @@
 
 module clock_gen ();
 	reg clk;
-
+	
+	// block #1
 	initial
-		clk = 1'b0;
-
+		clk = 1'b0; // t=0
+	// block #2
 	always
 		#5 clk = !clk;
-
+	// block #3
+	/*
 	initial
 		#100 $finish;
-
+	*/
 endmodule
